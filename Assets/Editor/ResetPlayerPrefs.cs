674ad73acd27e860297a94ac878c14d0c7184d71
Gijs <g.vandenbiggelaar@gmail.com>
2019-01-24 12:22:22 +0100

    Added Pixel Perfect Camera (Package)
    Also played around with a bunch of settings

---
+++ Assets/Editor/ResetPlayerPrefs.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ResetPlayerPrefs : MonoBehaviour
+{
+    [MenuItem("TTE/Reset PlayerPrefs")]
+    public static void DeletePlayerPrefs()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+}
