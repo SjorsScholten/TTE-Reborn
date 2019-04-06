1892509145d044f08d6b06b7eddcc8b218dd0acc
Sjors Scholten <scholtensjors22@gmail.com>
2019-04-06 23:41:55 +0200

    added activateable interface

---
+++ Assets/Scripts/Utils/Interactables/IActivateable.cs
@@ -0,0 +1,9 @@
+﻿public interface IActivateable {
+    
+    /**
+     * everything that can be activated, player cant interact with this, but an interactable can
+     * @param nothing
+     * @return nothing
+     */
+    void Activate();
+}
