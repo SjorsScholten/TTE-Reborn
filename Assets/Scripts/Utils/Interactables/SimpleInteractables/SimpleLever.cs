d94492e90b521df3427f1671aefd1c3d97ba324d
Sjors Scholten <scholtensjors22@gmail.com>
2019-04-06 23:42:11 +0200

    added simple lever

---
+++ Assets/Scripts/Utils/Interactables/SimpleInteractables/SimpleLever.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class SimpleLever : MonoBehaviour, IInteractable {
+
+    [SerializeField]
+    private GameObject[] activateables;
+    
+    public void Interact() {
+        Switch();
+    }
+
+    private void Switch() {
+        foreach (var activateable in activateables) {
+            activateable.GetComponent<IActivateable>().Activate();
+        }
+    }
+}
