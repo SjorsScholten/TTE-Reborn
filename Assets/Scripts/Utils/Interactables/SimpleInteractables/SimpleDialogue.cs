12a6f3329349d356d36a8389e5f18c8231d6c297
Sjors Scholten <scholtensjors22@gmail.com>
2019-04-06 23:41:35 +0200

    added a simple dialogue interaction

---
+++ Assets/Scripts/Utils/Interactables/SimpleInteractables/SimpleDialogue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public class SimpleDialogue : MonoBehaviour, IInteractable {
+
+    [SerializeField] [TextArea]
+    private String dialogue;
+
+    public void Interact() {
+        OpenDialogue();
+    }
+
+    private void OpenDialogue() {
+        //todo: show dialogue on screen
+    }
+}
