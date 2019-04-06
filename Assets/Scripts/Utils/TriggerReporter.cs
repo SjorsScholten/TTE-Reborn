47d50254abbb7e385670153be0e5c0b331f94bca
Gijs <g.vandenbiggelaar@gmail.com>
2019-04-06 15:56:22 +0200

    Tags enum class

---
+++ Assets/Scripts/Utils/TriggerReporter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerReporter : MonoBehaviour {
+
+    [SerializeField]
+    private bool onlyTriggerOnce;
+
+    [Serializable]
+    public class OnTriggerEnter : UnityEvent<Collider2D> { }
+    public OnTriggerEnter onTriggerEnterEvent;
+
+    [Serializable]
+    public class OnTriggerExit : UnityEvent<Collider2D> { }
+    public OnTriggerExit onTriggerExitEvent;
+
+    [SerializeField]
+    private bool ignoreEnemies;
+
+    void Start() {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (ignoreEnemies && other.CompareTag(Tags.Enemies.ToString())) {
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        
+    }
+}
