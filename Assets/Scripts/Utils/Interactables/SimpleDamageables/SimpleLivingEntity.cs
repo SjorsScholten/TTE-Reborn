3d8b7c1c2719c6c68c92a8425c56c441b49c3e55
Sjors Scholten <scholtensjors22@gmail.com>
2019-04-07 13:35:34 +0200

    added simple damageable

---
+++ Assets/Scripts/Utils/Interactables/SimpleDamageables/SimpleLivingEntity.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleLivingEntity : MonoBehaviour, IDamageable {
+    [SerializeField]
+    private float startingHealth;
+    private float currentHealth;
+    private bool dead = false;
+
+    private void Awake() {
+        currentHealth = startingHealth;
+    }
+
+    public void TakeHit(float damage) {
+        currentHealth -= damage;
+        if (currentHealth <= 0 && !dead) {
+            Die();
+        }
+    }
+
+    private void Die() {
+        dead = true;
+    }
+}
