015d0cb8487d9ccdb47ee4225a8978af0aaee426
Sjors Scholten <scholtensjors22@gmail.com>
2019-04-07 13:35:03 +0200

    added simple destroyable

---
+++ Assets/Scripts/Utils/Interactables/SimpleDestroyables/SimpleDestroyable.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleDestroyable : MonoBehaviour, IDestroyable{
+    public void Destroy() {
+        Destroy(gameObject);
+    }
+}
