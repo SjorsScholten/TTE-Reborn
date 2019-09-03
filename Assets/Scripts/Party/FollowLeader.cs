672537cb1140db250aa40396ffe8ccd8ecb56c9a
Gijs <g.vandenbiggelaar@gmail.com>
2019-09-03 15:22:54 +0200

    beginning

---
+++ Assets/Scripts/Party/FollowLeader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowLeader : MonoBehaviour {
+
+    public Transform target;
+
+    [SerializeField] private float distanceRadius;
+
+    private float _maxVelocity;
+    private Vector3 _velocity;
+    private Vector3 otherVelocity;
+
+    void Start() {
+        _maxVelocity = GetComponent<MultidirectionalTransformMovement>().MoveSpeed;
+    }
+
+    void Update() {
+        _velocity = (target.position - transform.position).normalized * _maxVelocity;
+
+        if (Vector2.Distance(target.position, transform.position) > distanceRadius) {
+            _velocity = Vector3.SmoothDamp(_velocity, Vector3.zero, ref otherVelocity, 0.7f);
+            transform.position += _velocity * Time.deltaTime;
+        } 
+    }
+
+}
