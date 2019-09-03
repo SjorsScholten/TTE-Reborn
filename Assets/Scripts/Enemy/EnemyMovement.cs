503d93246495680df5bc9e46b7083966d8cce41b
Olaf <o.olaf@hotmail.com>
2019-09-03 15:03:25 +0200

    Enemy now chooses a target
    - Chooses a target that is the closest and in vision of the enemy.

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -21,15 +21,21 @@ public class EnemyMovement : MonoBehaviour
     [Header("Events")]
     public OnMoveEnemy OnMoveEnemyEvent;
 
+    #region Walk Direction
     private Vector2 direction;
     private Vector2 lastDirection;
+    #endregion
 
+    #region Targets
+    private List<Transform> possibleTargets;
     private Transform target;
-    
+    #endregion
+
     private Vector3 debugDirection;
 
     private void Awake()
     {
+        possibleTargets = new List<Transform>();
         lastDirection = Vector2.down;
     }
 
@@ -38,32 +44,23 @@ public class EnemyMovement : MonoBehaviour
     {
         if (target != null)
         {
-            //Set positions
-            Vector3 origin = this.transform.position;
-            Vector3 directionVector3 = target.position - origin;
-
-            //Debug
-            debugDirection = directionVector3;
-
-            //If distance > minDistance then move on
-            if (directionVector3.magnitude > minDistance)
-            {
-                
-                Vector2 directionV2 = new Vector2(directionVector3.x, directionVector3.y);
-
-                RaycastHit2D hit = Physics2D.Raycast(new Vector2(origin.x, origin.y), directionV2, 10, layerMask.value);
-
-                //When a playable is found walk in that direction.
-                if (hit.transform.tag == NYRA.Tag.Playable)
-                {
-                    this.direction = directionV2;
-                    DoMove();
-                }
-            }
+            TryTarget(target);
 
         }
         else
         {
+            if (possibleTargets.Count > 0)
+            {
+                foreach (Transform t in possibleTargets)
+                {
+                    if (TryTarget(t))
+                    {
+                        return;
+                    }
+                }
+            }
+
+            //This code only runs if there are no targets.
             //Debug
             debugDirection = Vector3.zero;
 
@@ -72,6 +69,39 @@ public class EnemyMovement : MonoBehaviour
             animator.SetFloat("Horizontal", lastDirection.x);
             animator.SetFloat("Vertical", lastDirection.y);
         }
+    }
+
+    private bool TryTarget(Transform t)
+    {
+        //Set positions
+        Vector3 origin = this.transform.position;
+        Vector3 directionVector3 = t.position - origin;
+
+        //Debug
+        debugDirection = directionVector3;
+
+        //If distance > minDistance then move on
+        if (directionVector3.magnitude > minDistance)
+        {
+
+            Vector2 directionV2 = new Vector2(directionVector3.x, directionVector3.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(new Vector2(origin.x, origin.y), directionV2, 10, layerMask.value);
+
+            //When a playable is found walk in that direction.
+            if (hit.transform.tag == NYRA.Tag.Playable)
+            {
+                this.direction = directionV2;
+                DoMove();
+                return true;
+            }
+        }
+        else
+        {
+            return true;
+        }
+        return false;
+
     }
 
     /// <summary>
@@ -92,9 +122,9 @@ public class EnemyMovement : MonoBehaviour
     /// <param name="other"></param>
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag == NYRA.Tag.Playable)
+        if(other.tag == NYRA.Tag.Playable && !possibleTargets.Contains(other.transform))
         {
-            target = other.transform;
+            possibleTargets.Add(other.transform);
         }
     }
 
@@ -104,9 +134,9 @@ public class EnemyMovement : MonoBehaviour
     /// <param name="other"></param>
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.tag == NYRA.Tag.Playable)
+        if (other.tag == NYRA.Tag.Playable && possibleTargets.Contains(other.transform))
         {
-            target = null;
+            possibleTargets.Remove(other.transform);
         }
     }
 
