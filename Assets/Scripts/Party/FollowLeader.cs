de8570c140ebea7cfd621c18705f0e98be7ed20f
Gijs <g.vandenbiggelaar@gmail.com>
2019-09-09 12:03:00 +0200

    earthbound style following

--- Assets/Scripts/Party/FollowLeader.cs
+++ Assets/Scripts/Party/FollowLeader.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FollowLeader : MonoBehaviour {
 
+<<<<<<< Updated upstream
     public Transform target;
 
     [SerializeField] private float distanceRadius;
@@ -12,11 +14,18 @@ public class FollowLeader : MonoBehaviour {
     private Vector3 _velocity;
     private Vector3 otherVelocity;
 
+=======
+    public List<Transform> partyMembers;
+
+    [SerializeField] private float distanceRadius;
+
+>>>>>>> Stashed changes
     void Start() {
         _maxVelocity = GetComponent<MultidirectionalTransformMovement>().MoveSpeed;
     }
 
     void Update() {
+<<<<<<< Updated upstream
         _velocity = (target.position - transform.position).normalized * _maxVelocity;
 
         if (Vector2.Distance(target.position, transform.position) > distanceRadius) {
@@ -26,3 +35,30 @@ public class FollowLeader : MonoBehaviour {
     }
 
 }
+=======
+        for (int i = 1; i < partyMembers.Count; i++) {
+            var member = partyMembers[i].transform;
+            var target = partyMembers[i - 1].transform;
+
+            var maxVelocity = member.GetComponent<MultidirectionalTransformMovement>().MoveSpeed;
+            var animator = member.GetComponent<Animator>();
+            var velocity = (target.position - member.position).normalized * maxVelocity;
+
+            if (Vector2.Distance(target.position, member.position) > distanceRadius) {
+                member.position += velocity * Time.deltaTime;
+            }
+
+            AnimateFollower(member, velocity);
+        }
+    }
+
+    private void AnimateFollower(Transform member, Vector3 velocity) {
+        var animator = member.GetComponent<Animator>();
+        var targetAnimator = partyMembers[0].GetComponent<Animator>();
+
+        animator.SetBool("Idle", targetAnimator.GetBool("Idle"));
+        animator.SetFloat("Horizontal", velocity.x);
+        animator.SetFloat("Vertical", velocity.y);
+    }
+}
+>>>>>>> Stashed changes
