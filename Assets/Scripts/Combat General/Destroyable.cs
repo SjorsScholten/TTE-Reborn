2e5b030e5762e2691989adaebe351b084b1ef706
Gijs <g.vandenbiggelaar@gmail.com>
2019-09-17 13:02:15 +0200

    added events to destroyable

--- Assets/Scripts/Combat General/Destroyable.cs
+++ Assets/Scripts/Combat General/Destroyable.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Destroyable : MonoBehaviour {
 
@@ -10,13 +11,30 @@ public class Destroyable : MonoBehaviour {
 
     [SerializeField] [EnumFlag] private DamageSource immuneToSource;
 
+    [Serializable] public class OnDestroyed : UnityEvent<Transform> { }
+    public OnDestroyed OnDestroyedEvent;
+
+    [Serializable] public class OnDamaged : UnityEvent { }
+    public OnDamaged OnDamagedEvent;
+
     public bool Damage(int baseDamage, Transform origin, DamageSource source) {
         if (IsImmune(source)) return false;
         int defense = DefenseOrResistance(source);
 
         int damage = baseDamage - defense;
+        
+        //TODO: Health Calculation and Updates
+        //Apply Knockback?
+        //Damage Effects
+        //Check If Dead
+
+        OnDamagedEvent.Invoke();
 
         return true;
+    }
+
+    private void DestroyDestroyable() {
+        OnDestroyedEvent.Invoke(transform);
     }
 
     private bool IsImmune(DamageSource source) {
