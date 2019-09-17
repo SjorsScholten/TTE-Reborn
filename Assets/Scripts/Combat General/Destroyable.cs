e43dda258dd4b2effdbcce06e8bb9951b0a3140e
Gijs <g.vandenbiggelaar@gmail.com>
2019-09-17 12:49:47 +0200

    base for destroyables
    also renamed partymemberstats to basestats

---
+++ Assets/Scripts/Combat General/Destroyable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyable : MonoBehaviour {
+
+    [SerializeField] private BaseStats stats;
+    public BaseStats Stats { get { return stats; } }
+
+    [SerializeField] [EnumFlag] private DamageSource immuneToSource;
+
+    public bool Damage(int baseDamage, Transform origin, DamageSource source) {
+        if (IsImmune(source)) return false;
+        int defense = DefenseOrResistance(source);
+
+        int damage = baseDamage - defense;
+
+        return true;
+    }
+
+    private bool IsImmune(DamageSource source) {
+        return immuneToSource.HasFlag(source);
+    }
+
+    private int DefenseOrResistance(DamageSource source) {
+        if ((source & DamageSource.Magic) != 0) return stats.resistance;
+        else return stats.defense;
+    }
+
+}
---
+++ Assets/Scripts/Enums/DamageSource.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+[System.Flags]
+public enum DamageSource {
+    Sword = 1 << 0,
+    Bow = 1 << 1,
+    Fire = 1 << 2,
+    Water = 1 << 3,
+    Wood = 1 << 4,
+    Metal = 1 << 5,
+    Earth = 1 << 6,
+    Flail = 1 << 7,
+    Punch = 1 << 8,
+    Magic = Fire | Water | Wood | Metal | Earth
+}
