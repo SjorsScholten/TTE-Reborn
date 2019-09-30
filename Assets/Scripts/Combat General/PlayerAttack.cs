6991f91756f8726adff3f6de395fde8a888bf454
Gijs <g.vandenbiggelaar@gmail.com>
2019-09-30 16:11:07 +0200

    added unity ui

--- Assets/Scripts/Combat General/PlayerAttack.cs
+++ Assets/Scripts/Combat General/PlayerAttack.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerAttack : MonoBehaviour, PlayerControls.ICombatActions {
 
